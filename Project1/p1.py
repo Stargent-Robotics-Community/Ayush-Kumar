1529b6c5a832336aa067ce8a16279eb04bcd980d
ayush kumar <ayushkumar.it24@jecrc.ac.in>
2021-07-07 14:45:50 +0530

    first project done

---
+++ Project1/p1.py
@@ -0,0 +1,7 @@
+myfav = [ "ironman" ,"thor", "john wick" ,"money  heist"]
+for i in range(0,2):
+    n=input("enter your fav show")
+
+    myfav.append(n)
+
+print(myfav)
---
+++ Project1/p2.py
@@ -0,0 +1,5 @@
+myfav = [ "ironman" ,"thor", "john wick" ,"money  heist"]
+
+for i in myfav:
+    print(i)
+
